605bdb29e3914ddcc9372d48f75c85d36eaf6a5e
AlipYusri <alips400@gmail.com>
2024-11-28 10:22:54 +0800

    first commit

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'), // The password will be 'password'
+
+            'remember_token' => Str::random(10),
+        ])->assignRole('admin');
+    }
+}
